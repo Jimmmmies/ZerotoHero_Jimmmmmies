8db37504f170910ddc1295346c2731def414e604
Jimmmmies <2099869463@qq.com>
2025-08-15 21:56:36 +0800

    update learning progress

--- makemore/BackpropNinja.py
+++ makemore/BackpropNinja.py
@@ -41,3 +41,118 @@ def cmp(s, dt, t):
   maxdiff = (dt - t.grad).abs().max().item()
   print(f'{s:15s} | exact: {str(ex):5s} | approximate: {str(app):5s} | maxdiff: {maxdiff}')
   
+n_embd = 10 
+n_hidden = 200
+g = torch.Generator().manual_seed(2147483647)
+C  = torch.randn((vocab_size, n_embd), generator=g)
+W1 = torch.randn((n_embd * block_size, n_hidden), generator=g) * (5/3)/((n_embd * block_size)**0.5)
+b1 = torch.randn(n_hidden, generator=g) * 0.1
+W2 = torch.randn((n_hidden, vocab_size), generator=g) * 0.1
+b2 = torch.randn(vocab_size, generator=g) * 0.1
+bngain = torch.randn((1, n_hidden)) * 0.1 + 1.0
+bnbias = torch.randn((1, n_hidden)) * 0.1
+
+parameters = [C, W1, b1, W2, b2, bngain, bnbias]
+for p in parameters:
+    p.requires_grad = True
+    
+max_steps = 200000
+batch_size = 32
+n = batch_size
+lossi = []
+
+with torch.no_grad():
+    for step in range(max_steps):
+        ix = torch.randint(0, Xtr.shape[0], (batch_size,), generator=g)
+        Xb, Yb = Xtr[ix], Ytr[ix]
+        
+        emb = C[Xb]
+        embcat = emb.view(emb.shape[0], -1)
+        hprebn = embcat @ W1 + b1
+        
+        bnmean = hprebn.mean(dim=0, keepdims=True)
+        bnvar = hprebn.var(dim=0, keepdims=True, unbiased=True)
+        bnvar_inv = (bnvar + 1e-5)**-0.5
+        bnraw = (hprebn - bnmean) * bnvar_inv
+        hpreact = bngain * bnraw + bnbias
+        
+        h = torch.tanh(hpreact)
+        logits = h @ W2 + b2
+        loss = F.cross_entropy(logits, Yb)
+        
+        for p in parameters:
+            p.grad = None
+            
+        dlogits = F.softmax(logits, 1)
+        dlogits[range(n), Yb] -= 1
+        dlogits /= n
+        
+        dh = dlogits @ W2.T
+        dW2 = h.T @ dlogits
+        db2 = dlogits.sum(0)
+        dhpreact = (1.0 - h**2) * dh
+        
+        dbngain = (bnraw * dhpreact).sum(0, keepdim=True)
+        dbnbias = dhpreact.sum(0, keepdim=True)
+        dhprebn = bngain * bnvar_inv/n * (n * dhpreact - dhpreact.sum(0) - n/(n-1) * bnraw * (dhpreact * bnraw).sum(0))
+        
+        dembcat = dhprebn @ W1.T
+        dW1 = embcat.T @ dhprebn
+        db1 = dhprebn.sum(0)
+
+        demb = dembcat.view(emb.shape)
+        dC = torch.zeros_like(C)
+        for i in range(emb.shape[0]):
+            for j in range(emb.shape[1]):
+                dC[Xb[i, j]] += demb[i, j]
+        grads = [dC, dW1, db1, dW2, db2, dbngain, dbnbias]
+        
+        lr = 0.1 if step < 100000 else 0.01
+        for p, grad in zip(parameters, grads):
+            p.data -= lr * grad
+
+with torch.no_grad():
+    emb = C[Xtr]
+    embcat = emb.view(emb.shape[0], -1)
+    hpreact = embcat @ W1 + b1
+    bnmean = hpreact.mean(0, keepdim=True)
+    bnvar = hpreact.var(0, keepdim=True, unbiased=True)
+            
+@torch.no_grad()
+def split_loss(split):
+    x,y = {
+    'train': (Xtr, Ytr),
+    'val': (Xdev, Ydev),
+    'test': (Xte, Yte),
+  }[split]
+    emb = C[x]
+    embcat = emb.view(emb.shape[0], -1)
+    hpreact = embcat @ W1 + b1
+    hpreact = bngain * (hpreact - bnmean) * (bnvar + 1e-5)**-0.5 + bnbias
+    h = torch.tanh(hpreact)
+    logits = h @ W2 + b2
+    loss = F.cross_entropy(logits, y)
+    print(split, loss.item())
+    
+split_loss('train')
+split_loss('val')
+
+g = torch.Generator().manual_seed(2147483647 + 10)
+for _ in range(20):
+
+    out = []
+    context = [0] * block_size
+    while True:
+      emb = C[torch.tensor([context])] 
+      embcat = emb.view(emb.shape[0], -1)
+      hpreact = embcat @ W1 + b1
+      hpreact = bngain * (hpreact - bnmean) * (bnvar + 1e-5)**-0.5 + bnbias
+      h = torch.tanh(hpreact)
+      logits = h @ W2 + b2
+      probs = F.softmax(logits, dim=1)
+      ix = torch.multinomial(probs, num_samples=1, generator=g).item()
+      context = context[1:] + [ix]
+      out.append(ix)
+      if ix == 0:
+        break
+    print(''.join(itos[i] for i in out))
